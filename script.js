dc4bad474ed36eb5f029ac684939353de6b2d711
Saintwq <130616689+saintzada@users.noreply.github.com>
2023-09-20 13:31:06 -0700

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,26 @@ products.forEach((product) => {
         buyButton.style.opacity = '0';
     });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const bannerImages = document.querySelectorAll(".banner-img");
+    let currentIndex = 0;
+
+    function showImage(index) {
+        bannerImages.forEach((img, i) => {
+            if (i === index) {
+                img.classList.add("active");
+            } else {
+                img.classList.remove("active");
+            }
+        });
+    }
+
+    function nextImage() {
+        currentIndex = (currentIndex + 1) % bannerImages.length;
+        showImage(currentIndex);
+    }
+
+    // Iniciar a rotação automática
+    setInterval(nextImage, 3000); // Mude de imagem a cada 3 segundos (3000 milissegundos)
+});
