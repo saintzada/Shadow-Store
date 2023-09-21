fa74680709407a0610f941b37afae46660340e24
Saintwq <130616689+saintzada@users.noreply.github.com>
2023-09-20 18:18:16 -0700

    Update script.js

--- script.js
+++ script.js
@@ -35,3 +35,24 @@ document.addEventListener("DOMContentLoaded", function () {
     // Iniciar a rotação automática
     setInterval(nextImage, 3000); // Mude de imagem a cada 3 segundos (3000 milissegundos)
 });
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let slides = document.querySelectorAll('.slide');
+    
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+    
+    slideIndex++;
+    
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    
+    slides[slideIndex - 1].style.display = 'block';
+    
+    setTimeout(showSlides, 2000); // Muda a imagem a cada 2 segundos
+}
