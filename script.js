c9d951af09aa9988b0d04b0cfe51342b0a272b92
Saintwq <130616689+saintzada@users.noreply.github.com>
2023-09-22 19:23:40 -0700

    Update script.js

--- script.js
+++ script.js
@@ -20,39 +20,39 @@ document.addEventListener("DOMContentLoaded", function () {
     function showImage(index) {
         bannerImages.forEach((img, i) => {
             if (i === index) {
-                img.classList.add("active");
+                img.style.opacity = "1"; // Torna a imagem visível
             } else {
-                img.classList.remove("active");
+                img.style.opacity = "0"; // Torna a imagem invisível
             }
         });
     }
 
     function nextImage() {
+        const previousIndex = currentIndex;
         currentIndex = (currentIndex + 1) % bannerImages.length;
         showImage(currentIndex);
+        setTimeout(() => {
+            bannerImages[previousIndex].style.transition = "opacity 0.5s";
+            bannerImages[previousIndex].style.opacity = "0";
+            setTimeout(() => {
+                bannerImages[previousIndex].style.transition = "";
+            }, 500); // Remover a transição após 0.5 segundos
+        }, 0); // Adiar a transição da imagem anterior em 0 segundos
     }
+
+    // Adicionar evento de mouseover para as imagens do slide
+    bannerImages.forEach((img, i) => {
+        img.addEventListener("mouseover", () => {
+            img.style.opacity = "1"; // Torna a imagem visível ao passar o mouse
+        });
+
+        img.addEventListener("mouseout", () => {
+            if (i !== currentIndex) {
+                img.style.opacity = "0"; // Torna a imagem invisível quando o mouse sai, exceto se for a imagem atual
+            }
+        });
+    });
 
     // Iniciar a rotação automática
     setInterval(nextImage, 3000); // Mude de imagem a cada 3 segundos (3000 milissegundos)
 });
-
-let slideIndex = 0;
-showSlides();
-
-function showSlides() {
-    let slides = document.querySelectorAll('.slide');
-    
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = 'none';
-    }
-    
-    slideIndex++;
-    
-    if (slideIndex > slides.length) {
-        slideIndex = 1;
-    }
-    
-    slides[slideIndex - 1].style.display = 'block';
-    
-    setTimeout(showSlides, 2000); // Muda a imagem a cada 2 segundos
-}
